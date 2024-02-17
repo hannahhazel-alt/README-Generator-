ef6cf1cecb55405076408d8b11df54f4e7b9d035
Hannah Hazel <hazelhannah48@gmail.com>
2024-02-17 17:56:42 +0000

    added code to generateMarkdown.js file and fixed bugs in index.js file

--- index.js
+++ index.js
@@ -8,65 +8,65 @@ import generateMarkdown from "./utils/generateMarkdown";
 //creating an array of questions for user input// 
 const questions = [
     {
-        type: "input"
-        name: "title"
-        message: "Please name your project"
+        type: "input",
+        name: "title",
+        message: "Please name your project",
     },
     {
-        type: "input"
-        name: "description"
-        message: "Please describe this project"
+        type: "input",
+        name: "description",
+        message: "Please describe this project",
     },
     {
-        type: "input"
-        name: "screenshot"
-        message: "Please provide the relative path to a screenshot of the application"
+        type: "input",
+        name: "screenshot",
+        message: "Please provide the relative path to a screenshot of the application",
     },
     {
-        type: "input"
-        name: "link"
-        message: "Please provide a URL to the deployed app for users to access"
+        type: "input",
+        name: "link",
+        message: "Please provide a URL to the deployed app for users to access",
     },
     {
-        type: "checkbox"
-        name: "license"
-        message: "Please select a license applicable to this project"
+        type: "checkbox",
+        name: "license",
+        message: "Please select a license applicable to this project",
         choices: ["MIT", "none"],
     },
     {
-        type: "input"
-        name: "require"
-        message: "Please list any dependencies here"
+        type: "input",
+        name: "require",
+        message: "Please list any dependencies here",
     },
     {
-        type: "input"
-        name: "features"
-        message: "Please list any cool features here"
+        type: "input",
+        name: "features",
+        message: "Please list any cool features here",
     },
     {
-        type: "input"
-        name: "usage"
-        message: "Please list any languages used for this project"
+        type: "input",
+        name: "usage",
+        message: "Please list any languages used for this project",
     },
     {
-        type: "input"
-        name: "creator"
-        message: "What is your Github Username?"
+        type: "input",
+        name: "creator",
+        message: "What is your Github Username?",
     },
     {
-        type: "input"
-        name: "email"
-        message: "What is your email?"
+        type: "input",
+        name: "email",
+        message: "What is your email?",
     },
     {
-        type: "input"
-        name: "contributers"
-        message: "Please list any contributers using their Github usernames"
+        type: "input",
+        name: "contributers",
+        message: "Please list any contributers using their Github usernames",
     },
     {
-        type: "input"
-        name: "test"
-        message: "What tests should be run?"
+        type: "input",
+        name: "test",
+        message: "What tests should be run?",
     },
 ]
 
