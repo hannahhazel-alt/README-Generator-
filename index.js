f6ec1ef71a4cf8b073b78122493845252577e5cd
Hannah Hazel <hazelhannah48@gmail.com>
2024-02-17 17:28:42 +0000

    added my questions to index.js file with a function for initialsing app and writing the file

---
+++ index.js
@@ -0,0 +1,85 @@
+//including packages needed// 
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import generateMarkdown from "./utils/generateMarkdown"; 
+
+//creating an array of questions for user input// 
+const questions = [
+    {
+        type: "input"
+        name: "title"
+        message: "Please name your project"
+    },
+    {
+        type: "input"
+        name: "description"
+        message: "Please describe this project"
+    },
+    {
+        type: "input"
+        name: "screenshot"
+        message: "Please provide the relative path to a screenshot of the application"
+    },
+    {
+        type: "input"
+        name: "link"
+        message: "Please provide a URL to the deployed app for users to access"
+    },
+    {
+        type: "checkbox"
+        name: "license"
+        message: "Please select a license applicable to this project"
+        choices: ["MIT", "none"],
+    },
+    {
+        type: "input"
+        name: "require"
+        message: "Please list any dependencies here"
+    },
+    {
+        type: "input"
+        name: "features"
+        message: "Please list any cool features here"
+    },
+    {
+        type: "input"
+        name: "usage"
+        message: "Please list any languages used for this project"
+    },
+    {
+        type: "input"
+        name: "creator"
+        message: "What is your Github Username?"
+    },
+    {
+        type: "input"
+        name: "email"
+        message: "What is your email?"
+    },
+    {
+        type: "input"
+        name: "contributers"
+        message: "Please list any contributers using their Github usernames"
+    },
+    {
+        type: "input"
+        name: "test"
+        message: "What tests should be run?"
+    },
+]
+
+//initialising app 
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md file...");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses}));
+    });
+}
+init();
+
+//writing README.md file 
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
